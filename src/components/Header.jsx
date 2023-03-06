1ea08ac4309bbdf461997c3fdf06fe84473da9d6
Ashutosh <porwal0403@gmail.com>
2023-03-06 23:58:19 +0530

    Initial Project Setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import classes from "./header.module.scss";
+
+function Header() {
+  return (
+    <div className={classes.header}>
+      <p>Hello World.</p>
+    </div>
+  );
+}
+
+export default Header;
